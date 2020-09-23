b2814b3db76cadb5906a47633b9e7aa8e4dceb31
Sta-sky <56905658+Sta-sky@users.noreply.github.com>
2020-09-23 14:27:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 
+from flask import Flask
+from flask_test.login.views import login
+from flask_test.login.views import check_login
+
+
+app = Flask(
+    __name__,
+    template_folder='static/templates',
+    static_folder='static')
+app.register_blueprint(login, url_prefix='/user')
+app.register_blueprint(check_login, url_prefix='/user')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
